3bd39cb47455d7795ecab1071d715e6ce70597ba
Agustincma <agustin.cardozo001@gmail.com>
2026-02-12 23:10:33 -0300

    add card in home for testing

--- app/home/Home.tsx
+++ app/home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MainCard from '../components/cards/MainCard'
 
 const Main = () => {
   return (
     <div>
-      <h1>Main</h1>
+      <MainCard title={'Word RDC'}/>
     </div>
   )
 }
