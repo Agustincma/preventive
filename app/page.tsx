e54130853b00fdd1759bfd6354d16a45543e6a62
Agustincma <agustin.cardozo001@gmail.com>
2026-02-12 22:15:46 -0300

    feat: Inicializacion del proyecto

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Main from "./home/Home";
+
+export default function Home() {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+}
---
+++ app/preventives/device/[device].tsx
@@ -0,0 +1,9 @@
+const PageDevice = () => {
+  return (
+    <div>
+      <h1>Pagina para los devices</h1>
+    </div>
+  )
+}
+
+export default PageDevice
